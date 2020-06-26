579a3670d53e2329825f5a2f56adc150e74866f7
Ihor Medvid <igormedvid89@gmail.com>
2020-06-26 21:53:14 +0300

    update.circle

--- src/main/java/ua/oop/Circle.java
+++ src/main/java/ua/oop/Circle.java
@@ -1,25 +1,8 @@
 package ua.oop;
 
-import static java.lang.Math.PI;
+public class Circle {
 
-public class Circle extends Figure{
-    private int radius;
-    private double result;
-
-    @Override
-    public void setRadius(int radious) {
-        if(this.radius > 0){
-            this.radius = radius;
-        }
-    }
-
-    @Override
-    public int getRadious() {
-        return radius;
-    }
-
-    @Override
-    public double Square(int radius) {
-        return result = Math.PI * Math.pow(this.radius, 2);
+    public double Square(double radius) {
+        return  Math.PI * Math.pow(radius, 2);
     }
 }
